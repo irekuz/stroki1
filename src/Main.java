88c7ac4cff7595f7c6fc78e66e4967f30f3e97e6
irekhusainov <i@khusainovirek>
2022-07-02 11:46:50 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание №1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — " + lastName + firstName + middleName);
+
+        //Задание №2
+
+       String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName1);
+
+        //Задание №3
+
+        String fullName3 = "Иванов Семён Семёнович";
+        fullName3 = fullName3.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника —" + fullName3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
